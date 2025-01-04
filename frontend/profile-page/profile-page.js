2290087f5943249ee4e591e1965fefa091c9c511
LisaMashkina <lisakaya919@gmail.com>
2025-01-04 12:53:34 +0300

    profile-page functions

--- frontend/profile-page/profile-page.js
+++ frontend/profile-page/profile-page.js
@@ -2,8 +2,11 @@ async function getResponce(){
     let response = await fetch('https://jsonplaceholder.typicode.com/photos')
     var content = await response.json()
     await console.log(content[content[1].id])
-    function cards(){
+    function works(){
         for(let i = 0; i < 10; i++){
+        document.querySelector('.orders').classList.add('hide')
+        document.querySelector('.skills').classList.add('hide')
+        document.querySelector('.works').classList.remove('hide')
         console.log(content[i].id)
         const card = document.createElement('div') //КАРТОЧКА
         card.classList.add('card')
@@ -24,12 +27,61 @@ async function getResponce(){
         
         }
     }
-    cards()
-//     function active_navbar (){
-        
-//     }
-}
+    works()
 
+    //при первом нажатии отработать, а потом просто hide 
+    function orders(){
+      for(let i = 0; i < 10; i++){
+      document.querySelector('.works').classList.add('hide')
+      document.querySelector('.skills').classList.add('hide')
+      document.querySelector('.orders').classList.remove('hide')
+      const card = document.createElement('div') //КАРТОЧКА
+      card.classList.add('card')
+      const img_in_card = document.createElement('img') //КАРТИНКА
+      img_in_card.classList.add('img_in_card')
+      img_in_card.src = "./otter.jpg"
+      const card_name = document.createElement('div') //НАЗВАНИЕ
+      card_name.innerHTML = content[i].title
+      card_name.classList.add('card_name')
+      const card_more = document.createElement('button') //ПОДРОБНЕЕ
+      card_more.classList.add('card_more') 
+      card_more.innerHTML = 'подробнее...'
+      
+      card.append(img_in_card)
+      card.append(card_name)
+      card.append(card_more)
+      document.querySelector('.orders').append(card)
+      
+      }
+    }
+
+
+
+    function active_navbar(){
+      const arr = document.querySelectorAll(".bt")
+      for(let i = 0; i < arr.length; i++){
+        arr[i].addEventListener("click", () => {
+          arr[i].classList.add("active")
+          for(let j = 0; j < arr.length; j++){
+            if(j != i){
+              arr[j].classList.remove("active")
+            }
+          }
+          if (i == 0){
+            works()
+            console.log('works')
+          }
+          if (i == 1){
+            orders()
+            console.log('orders')
+          }
+        })
+      }
+    
+    }
+    active_navbar()
+
+}
 getResponce()
 
 
